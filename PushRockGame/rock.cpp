5eef2de0e905820ecb04cbb5d3ba6c3978897908
1377254 <1377254@naver.com>
2019-11-21 00:31:21 +0900

    rock.cpp 파일 주석 추가

--- PushRockGame/rock.cpp
+++ PushRockGame/rock.cpp
@@ -9,6 +9,7 @@ Rock::Rock(const Pos& pos) : Ent(ROCK_AABB, pos, EntType::ROCK) {
 void Rock::draw() {
 	glPushMatrix();
 	{
+		// 참고로 바위의 크기는 0.9
 		glTranslatef(0.0, 0.45, 0.0); // 좌표축을 y축으로 0.45만큼 이다오
 		glutSolidCube(0.9); // 0.9 크기 만큼의 정육면체를 그린다.
 	}

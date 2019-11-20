765bfb131fbbc933886b7aceccf789dd33bddee5
2jun0 <soo28819@naver.com>
2019-11-21 00:34:58 +0900

    농담 추가 ㅋㅋㅋㅋ

--- PushRockGame/rock.cpp
+++ PushRockGame/rock.cpp
@@ -10,6 +10,7 @@ void Rock::draw() {
 	glPushMatrix();
 	{
 		// 참고로 바위의 크기는 0.9
+		// 바위는 영어로 Rock
 		glTranslatef(0.0, 0.45, 0.0); // 좌표축을 y축으로 0.45만큼 이다오
 		glutSolidCube(0.9); // 0.9 크기 만큼의 정육면체를 그린다.
 	}

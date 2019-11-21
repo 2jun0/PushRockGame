afde8f1974d8b26160ff86f5edaf1c6311ef0a09
1377254 <1377254@naver.com>
2019-11-21 10:30:38 +0900

    wall파일 수정

--- PushRockGame/wall.cpp
+++ PushRockGame/wall.cpp
@@ -9,7 +9,8 @@ Wall::Wall(Pos& pos): Ent(WALL_AABB, pos, EntType::WALL){
 void Wall::draw() {
 	glPushMatrix();
 	{
-		
+		glTranslatef(0.0, 0.5, 0.0); 
+		glutSolidCube(1.0);
 	}
 	glPopMatrix();
 }

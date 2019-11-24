5d4b92ab86e5d15a8562bbc8ac5f3dafc6b00248
lufan <1377254@naver.com>
2019-11-24 18:22:55 +0900

    로봇 그림을 그렸다

--- PushRockGame/player.cpp
+++ PushRockGame/player.cpp
@@ -7,5 +7,22 @@ Player::Player(const Pos& pos) : Ent(PLAYER_AABB, pos, EntType::PLAYER) {
 }
 
 void Player::draw() {
+	glPushMatrix();
+	glTranslatef(0.0, 0.4, 0.0);
+	glColor3f(1, 0, 0);
+	glScalef(0.4, 0.4, 0.4);
+	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslatef(-0.45, 0.3, 0.0);
+	glColor3f(1, 0.5, 0.2);
+	glScalef(0.1, 0.3, 0.1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.45, 0.3, 0.0);
+	glColor3f(1, 0.5, 0.2);
+	glScalef(0.1, 0.3, 0.1);
+	glPopMatrix();
 
 }

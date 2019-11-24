b181b8ef09f4b13086a0a1e3a085987983b3ca6c
2jun0 <soo28819@naver.com>
2019-11-24 18:22:03 +0900

    버튼 모델링 추가

--- PushRockGame/button.cpp
+++ PushRockGame/button.cpp
@@ -7,5 +7,21 @@ Button::Button(const Pos& pos) : Ent(BUTTON_AABB, pos, EntType::BUTTON) {
 }
 
 void Button::draw() {
+	glPushMatrix();
+	{
+		float vctPnt[][3] = {
+		{-0.5f,  -0.5f, 0.0f},
+		{-0.5f,  0.5f, 0.0f},
+		{ 0.5f, -0.5f, 0.0f},
+		{0.5f, 0.5f, 0.0f}
+		};
 
+		glBegin(GL_LINE_LOOP);
+		for (int i = 0; i < 4; i++)
+		{
+			glVertex3f(vctPnt[i][0], vctPnt[i][1], vctPnt[i][2]);
+		}
+		glEnd();
+	}
+	glPopMatrix();
 }

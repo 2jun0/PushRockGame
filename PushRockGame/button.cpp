16aa0d189a5abc6c2c725b824202cf8b629040bf
lufan <1377254@naver.com>
2019-11-26 20:56:51 +0900

    수정2

--- PushRockGame/button.cpp
+++ PushRockGame/button.cpp
@@ -1,5 +1,5 @@
 #include "button.h"
-const GLfloat Pi = 3.1415926536f;
+
 const Aabb Button::BUTTON_AABB = Aabb(-0.5, 0.5, -0.5, 0.5);
 
 Button::Button(const Pos& pos) : Ent(BUTTON_AABB, pos, EntType::BUTTON) {
@@ -9,11 +9,12 @@ Button::Button(const Pos& pos) : Ent(BUTTON_AABB, pos, EntType::BUTTON) {
 void Button::draw() {
 	glPushMatrix();
 	{
-		glBegin(GL_LINE_STRIP);
-		GLint n = 3600;
-		for (GLint i = 0; i < n; i++) {
-			glVertex2f(circ.x + radius * cos(twoPi * i / n), circ.y + radius * sin(twoPi * i / n));
-		}
+		glColor3f(0.0, 1.0, 1.0);
+		glBegin(GL_POLYGON);
+		glVertex3f(-0.5, -0.5, 0.0);
+		glVertex3f(0.5, -0.5, 0.0);
+		glVertex3f(0.5, 0.5, 0.0);
+		glVertex3f(-0.5 , 0.5, 0.0);
 		glEnd();
 
 	glPopMatrix();

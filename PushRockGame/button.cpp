06aedf3790675674658f57a62adeeafcb57acb6f
lufan <1377254@naver.com>
2019-11-26 21:17:02 +0900

    수정 3

--- PushRockGame/button.cpp
+++ PushRockGame/button.cpp
@@ -11,10 +11,10 @@ void Button::draw() {
 	{
 		glColor3f(0.0, 1.0, 1.0);
 		glBegin(GL_POLYGON);
-		glVertex3f(-0.5, -0.5, 0.0);
-		glVertex3f(0.5, -0.5, 0.0);
-		glVertex3f(0.5, 0.5, 0.0);
-		glVertex3f(-0.5, 0.5, 0.0);
+		glVertex3f(-0.5, 0.0,-0.5);
+		glVertex3f(0.5,0.0,-0.5);
+		glVertex3f(0.5, 0.0, 0.5);
+		glVertex3f(-0.5, 0.0, 0.5);
 		glEnd();
 	}
 	glPopMatrix();

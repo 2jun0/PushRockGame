9f1582af98468b64cebc64900377a9fa3ec3162c
lufan <1377254@naver.com>
2019-11-26 20:34:50 +0900

    button수정

--- PushRockGame/button.cpp
+++ PushRockGame/button.cpp
@@ -1,5 +1,5 @@
 #include "button.h"
-
+const GLfloat Pi = 3.1415926536f;
 const Aabb Button::BUTTON_AABB = Aabb(-0.5, 0.5, -0.5, 0.5);
 
 Button::Button(const Pos& pos) : Ent(BUTTON_AABB, pos, EntType::BUTTON) {
@@ -9,19 +9,12 @@ Button::Button(const Pos& pos) : Ent(BUTTON_AABB, pos, EntType::BUTTON) {
 void Button::draw() {
 	glPushMatrix();
 	{
-		float vctPnt[][3] = {
-		{-0.5f,  -0.5f, 0.0f},
-		{-0.5f,  0.5f, 0.0f},
-		{ 0.5f, -0.5f, 0.0f},
-		{0.5f, 0.5f, 0.0f}
-		};
-
-		glBegin(GL_LINE_LOOP);
-		for (int i = 0; i < 4; i++)
-		{
-			glVertex3f(vctPnt[i][0], vctPnt[i][1], vctPnt[i][2]);
+		glBegin(GL_LINE_STRIP);
+		GLint n = 3600;
+		for (GLint i = 0; i < n; i++) {
+			glVertex2f(circ.x + radius * cos(twoPi * i / n), circ.y + radius * sin(twoPi * i / n));
 		}
 		glEnd();
-	}
+
 	glPopMatrix();
 }

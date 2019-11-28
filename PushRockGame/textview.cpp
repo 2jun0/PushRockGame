f885607ee06502fba71cda67a57dec624fbbcf3f
lufan <1377254@naver.com>
2019-11-28 10:52:29 +0900

    오픈소스 추가했어(문자상관)

---
+++ PushRockGame/textview.cpp
@@ -0,0 +1,31 @@
+﻿#include "textview.h"
+#include <GL / glut.h> 
+#include <GL / glu.h> 
+#include <windows.h>
+TextView::TextView() { 
+
+}
+TextView::~TextView(){  
+
+}
+void TextView::drawString(char * str) {
+	intlen, i; 
+	wchar_t* wstring;
+	HDC hDC  = wglGetCurrentDC(); 
+	GLuint list  = glGenLists(1);
+	len  = 0;
+	for (i = 0; str[i] != '\0'; ++i) {
+		if (IsDBCSLeadByte(str[i]))            
+			++i;         
+		++len;
+	}
+	wstring  = (wchar_t*)malloc((len + 1) * sizeof(wchar_t));
+	MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, str, -1, wstring, len); 
+	wstring[len] = L'\0';
+	for (i = 0; i < len; ++i) {
+		wglUseFontBitmapsW(hDC, wstring[i], 1, list);
+		glCallList(list);
+	}
+			free(wstring);
+		glDeleteLists(list, 1);
+	}
---
+++ PushRockGame/textview.h
@@ -0,0 +1,5 @@
+﻿class TextView {
+	public:  TextView();
+			~TextView();
+			void drawString(char * str);
+}
